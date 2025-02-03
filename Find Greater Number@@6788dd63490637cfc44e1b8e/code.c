3f73941b0d3708f8a30a1483e15b27839d74aae9
Smriti-2006 <smriti0788.becse24@chitkara.edu.in>
2025-02-03 10:58:30 +0530

    Solution to Find Greater Number

---
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b ;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if (a>b){
+        printf(a);
+    }
+    else{
+        printf(b);
+    }
+    
+
+    return 0;
+}
